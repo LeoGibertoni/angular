d678da7284afa7e5be1679d8594dd6636d07c995
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-01-07 11:45:32 -0300

    Meu primeiro App

--- meu-primeiro-app/src/app/app.component.spec.ts
+++ meu-primeiro-app/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'meu-primeiro-app' title`, () => {
+  it(`should have as title 'meu-primeiro-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('meu-primeiro-app');
