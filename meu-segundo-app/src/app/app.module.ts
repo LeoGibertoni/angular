ebacc4deea22202fb8e4f052317ca114be61e076
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-01-07 12:39:32 -0300

    memoteca

---
+++ meu-segundo-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
