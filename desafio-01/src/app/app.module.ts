0df6561941221c04800f9dc292c161a87da489a3
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-01-08 10:38:43 -0300

    Desafio 01

---
+++ desafio-01/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Desafio01Component } from './desafio-01/desafio-01.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Desafio01Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ desafio-01/src/app/desafio-01/desafio-01.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desafio-01',
+  standalone: false,
+  
+  templateUrl: './desafio-01.component.html',
+  styleUrl: './desafio-01.component.css'
+})
+export class Desafio01Component {
+
+}
